7672e6bf6da2ffc17e52393d98899382c8f1e93b
Willian Zhang <Willian@Willian-Zhang.com>
2016-04-27 17:00:31 +0800

    add comments

--- spider.config.example.js
+++ spider.config.example.js
@@ -1,6 +1,8 @@
 module.exports = {
+    // fetched manually
     cookie:'',
     _xsrf:'',
+    // in number
     concurrency:1,
     // update threshold for fetching, in seconds
     updateThreshold:2592000
